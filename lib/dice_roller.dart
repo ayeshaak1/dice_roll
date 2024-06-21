e45f42d475db6dafeef9ff47769227cfaf9ab70f
Ayesha Khan <ayeshaa.k3002@gmail.com>
2024-06-21 03:33:55 -0400

    Optimize Random() to be global var to avoid reconstruction of object

--- lib/dice_roller.dart
+++ lib/dice_roller.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 
+final randomizer = Random();
+
 class DiceRoller extends StatefulWidget{
   const DiceRoller({super.key});
   
@@ -15,7 +17,7 @@ class _DiceRollerState extends State<DiceRoller>{
   
   void rollDice() {
     setState(() {
-      diceImg = Random().nextInt(6) + 1;
+      diceImg = randomizer.nextInt(6) + 1;
     });
   }
 
